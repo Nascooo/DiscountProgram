cf066db4b947e1305c017d14d04172a86a5bfe59
Nascooo <a.abdelnasser159@gmail.com>
2024-02-10 20:40:48 +0200

    Remove unNeeded property

--- src/main/java/org/discountprogram/DiscountProgramApplication.java
+++ src/main/java/org/discountprogram/DiscountProgramApplication.java
@@ -1,15 +1,10 @@
 package org.discountprogram;
 
-import org.discountprogram.repo.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiscountProgramApplication {
-
-    @Autowired
-    private UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(DiscountProgramApplication.class, args);
